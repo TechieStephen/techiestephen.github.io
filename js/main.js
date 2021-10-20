34c4d4658aa55b52ea399a4e33c242b5709f068a
ohien stephen <ohienstephen@gmail.com>
2021-10-20 15:37:56 +0100

    ------

--- js/main.js
+++ js/main.js
@@ -5,6 +5,11 @@ nav = document.getElementsByClassName('nav')
 
 const toggleMenu = () => {
     nav[0].classList.toggle('show')
+    if(nav.classList.contains('show')){
+        menu_bar.src = "images/icon-close.svg"
+    }else{
+        menu_bar.src = "images/icon-hamburger.svg"
+    }
 }
 
 menu.addEventListener('click', toggleMenu)
